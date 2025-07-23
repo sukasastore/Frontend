59fa80aa227f9cfbce3d7ce3277c8712563dab52
faizan tayyab <faizantayyab37@gmail.com>
2025-07-23 14:40:00 +0500

    Update environment configuration and URLs to reflect "Sukasa" branding. Replace all instances of the previous API URLs with the new "sukasastore.com" endpoints in both the admin and web environments. Adjust MiniCards component text to align with the new branding.

--- sukasa-web/environment.ts
+++ sukasa-web/environment.ts
@@ -5,15 +5,15 @@ export default function getEnv(env: "DEV" | "PROD") {
       // WS_SERVER_URL: "wss://enatega-multivendor-revamp.up.railway.app/",
       // SERVER_URL: "https://enatega-multivendor.up.railway.app/",
       // WS_SERVER_URL: "wss://enatega-multivendor.up.railway.app/",
-      SERVER_URL: "https://v1-api-enatega-single-multivendor.up.railway.app/",
-      WS_SERVER_URL: "wss://v1-api-enatega-single-multivendor.up.railway.app/",
+      SERVER_URL: "https://sukasastore.com/",
+      WS_SERVER_URL: "wss://sukasastore.com/",
       // SERVER_URL: "http://localhost:8001/",
       // WS_SERVER_URL: "ws://localhost:8001/",
     };
   } else {
     return {
-      SERVER_URL: "https://v1-api-enatega-single-multivendor.up.railway.app/",
-      WS_SERVER_URL: "wss://v1-api-enatega-single-multivendor.up.railway.app/",
+      SERVER_URL: "https://sukasastore.com/",
+      WS_SERVER_URL: "wss://sukasastore.com/",
       // SERVER_URL: "https://enatega-multivendor-revamp.up.railway.app/",
       // WS_SERVER_URL: "wss://enatega-multivendor-revamp.up.railway.app/",
     };
